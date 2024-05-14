3c126c3e486089c39d27eae57bcf724345187740
Mx-Iris <61279231+Mx-Iris@users.noreply.github.com>
2024-05-15 01:58:10 +0800

    Add Frida xcframework

---
+++ FridaTrack/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  FridaTrack
+//
+//  Created by JH on 2024/5/15.
+//
+
+import Foundation
+import Frida
+
+
+class ScriptDelegate: Frida.ScriptDelegate {
+    
+}
---
+++ FridaTrack/FridaTrack.h
@@ -0,0 +1,18 @@
+//
+//  FridaTrack.h
+//  FridaTrack
+//
+//  Created by JH on 2024/5/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FridaTrack.
+FOUNDATION_EXPORT double FridaTrackVersionNumber;
+
+//! Project version string for FridaTrack.
+FOUNDATION_EXPORT const unsigned char FridaTrackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FridaTrack/PublicHeader.h>
+
+
